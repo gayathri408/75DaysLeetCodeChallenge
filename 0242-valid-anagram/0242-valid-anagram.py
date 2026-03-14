9932f2efb456bf6d005fafdc6f0f223bc448eedd
gayathri408 <gayathrikanuri27@gmail.com>
2026-03-14 20:58:23 +0530

    Time: 27 ms (25.24%), Space: 13.5 MB (38.06%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        m=len(s)
+        n=len(t)
+        if len(s)!=len(t):
+            return False
+        if sorted(s) == sorted(t):
+            return True
+        return False
+
+
+        
