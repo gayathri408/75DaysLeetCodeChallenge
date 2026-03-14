312dcbfd98c2eea341f00c874087129a2037e0c9
gayathri408 <gayathrikanuri27@gmail.com>
2026-03-14 20:59:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,6 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        m=len(s)
-        n=len(t)
-        if len(s)!=len(t):
-            return False
-        if sorted(s) == sorted(t):
-            return True
-        return False
+        return sorted(s) == sorted(t)
 
 
         
